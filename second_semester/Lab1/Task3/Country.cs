9a0e84ad2f56542af16f530896b7183ad300f152
Artem <kolesnikovartem075@gmail.com>
2022-02-06 19:53:46 +0200

    Added Task3 and a little edits in project

---
+++ second_semester/Lab1/Task3/Country.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3;
+
+public class Country
+{
+    public string Name { get; }
+    public string CountyCode { get; }
+
+    public Country(string name, string countyCode)
+    {
+        Name = name;
+        CountyCode = countyCode;
+    }    
+    
+    public Country()
+    {
+        Name = "Default country";
+        CountyCode = "DF";
+    }
+}
