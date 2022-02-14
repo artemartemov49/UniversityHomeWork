30f89ed39456babe9e9acd8b5c79371ca0775ffc
Artem <kolesnikovartem075@gmail.com>
2022-02-14 14:15:46 +0200

    Added description on calculator

--- second_semester/Lab2/Task3/Calculator.cs
+++ second_semester/Lab2/Task3/Calculator.cs
@@ -4,6 +4,21 @@ using System.Text.RegularExpressions;
 
 namespace Task3;
 
+/*
+ * First of all we calculate priority operation (/*), the ordinary(+-)
+ *
+ * Find in string first symbol (/*)
+ * 3+4*5
+ * first priority symbol is *
+ * Find index of neighboring symbols and then take integers and operation char.
+ * We got: 4*5. Calculation result is 20.
+ * Then we replacing 4*5 in input string on our result. 
+ * After this cycle we have string to continue calculating: 3+20
+ *
+ * Repeat the steps until we get the final result
+ *
+ */
+
 public class Calculator : BasicCalculator
 {
     private static readonly char[] PriorityOperations =
@@ -38,8 +53,8 @@ public class Calculator : BasicCalculator
 
         while (charIndex != -1)
         {
-            var indexOfRightLastNumber = value.IndexOfAny(AllOperations, charIndex + 1);
             var indexOfLeftFirstNumber = value[..(charIndex - 1)].LastIndexOfAny(AllOperations);
+            var indexOfRightLastNumber = value.IndexOfAny(AllOperations, charIndex + 1);
 
             indexOfRightLastNumber = indexOfRightLastNumber == -1
                 ? value.Length
@@ -51,7 +66,7 @@ public class Calculator : BasicCalculator
             var operationChar = value[charIndex];
 
             var evaluationResult =
-                BasicCalculator.CalculateFormula(decimal.Parse(firstNumber), decimal.Parse(secondNumber), operationChar);
+                CalculateFormula(decimal.Parse(firstNumber), decimal.Parse(secondNumber), operationChar);
 
             value = value.Replace(
                 value.Substring(indexOfLeftFirstNumber + 1, indexOfRightLastNumber - indexOfLeftFirstNumber - 1),
